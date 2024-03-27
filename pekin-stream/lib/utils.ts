88c96fc11077829b842ad1e3627b75465f3a25bb
rahulgopireddy <rgopired@uncc.edu>
2024-03-27 12:54:32 -0400

    video and chat integrated

--- pekin-stream/lib/utils.ts
+++ pekin-stream/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const stringToColor = (str: String) => {
+  let hash = 0;
+
+  for (let index = 0; index < str.length; index++) {
+    hash = str.charCodeAt(1) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).substr(-2);
+  }
+
+  return color;
+};
